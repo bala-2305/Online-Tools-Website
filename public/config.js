5cff9787679f6c4cd8ad5b0547e7d90a565a5883
Mahibala <hmahibala@gmail.com>
2026-01-04 20:44:25 +0530

    anti

---
+++ public/config.js
@@ -0,0 +1,6 @@
+Object.defineProperty(window, 'API_URL', {
+    value: atob("aHR0cHM6Ly9uaW0tc2Vydi52ZXJjZWwuYXBwL2NoYXQ="),
+    enumerable: false,
+    configurable: false,
+    writable: false
+});
