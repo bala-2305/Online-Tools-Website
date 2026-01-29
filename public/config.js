475f0274a61c11483845dfd3836061e46e4a3670
Mahibala <hmahibala@gmail.com>
2026-01-29 22:15:41 +0530

    Initt

--- public/config.js
+++
@@ -1,6 +0,0 @@
-Object.defineProperty(window, 'API_URL', {
-    value: atob("aHR0cHM6Ly9uaW0tc2Vydi52ZXJjZWwuYXBwL2NoYXQ="),
-    enumerable: false,
-    configurable: false,
-    writable: false
-});
